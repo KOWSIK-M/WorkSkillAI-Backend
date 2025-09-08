2b6ea7b98a098668c66f7ee1891676af345a4b19
KOWSIK MEDAM <2200030358@klunniversity.in>
2025-09-08 16:27:16 +0530

    Created Project, Connected with MongoDB

---
+++ src/test/java/com/cp/workskillai/WorkskillaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cp.workskillai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkskillaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
