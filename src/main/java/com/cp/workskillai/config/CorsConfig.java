f7c3d6a315f1bba63a0d5a00ec9f68c8cab12bc3
KOWSIK MEDAM <2200030358@klunniversity.in>
2025-11-20 20:35:37 +0530

    very minor issue

--- src/main/java/com/cp/workskillai/config/CorsConfig.java
+++ src/main/java/com/cp/workskillai/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:8000","https://workskillai.onrender.com")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:8000","https://workskillai.onrender.com","https://workskillai-service.onrender.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
